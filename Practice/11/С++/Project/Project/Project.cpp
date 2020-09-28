8bf28a30b95fd39bab68942e9dd8a3db44100aee
Vakarev Maksim <rilst@ya.ru>
2020-09-28 11:24:42 +0300

    Practice 11 C++

---
+++ Practice/11/С++/Project/Project/Project.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    double a;
+    int b;
+    int i;
+    double c;
+
+    std::cout << "Введите число: \n";
+    std::cin >> a;
+    std::cout << "Введите необходимую степень числа \n";
+    std::cin >> b;
+        c = a;
+        for (i = 1; i < b; i++)
+        {
+            c = c * a;
+        }
+        std::cout << c;
+}
