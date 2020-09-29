40b8906c4588ca6e797c5ff92fe8b9aaf9e0d734
Vakarev Maksim <rilst@ya.ru>
2020-09-29 17:52:02 +0300

    Practice C++ 13

---
+++ Practice/13/С++/Project/Project/Project.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    float a;
+    int c = 0;
+    int i;
+
+    std::cout << "Введите целое число больше 1: ";
+    std::cin >> a;
+    std::cout << (int)a << std::endl << a << std::endl;
+    if ((int)a == a) {
+        if (a == 1 || a == 0) std::cout << "Неверно введенно число\n";
+        else if (a == 2) std::cout << "Простое\n";
+        else {
+            for (i = 2; i < a; i++)
+            {
+                if ((int)a % i == 0) { c = 1; i = a; };
+            }
+
+            if (c == 1) std::cout << "Составное\n";
+            else std::cout << "Простое\n";
+        }
+    }
+    else std::cout << "Необходимо вводить целое число!\n";
+}
