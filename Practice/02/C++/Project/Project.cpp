2ec9fc494ca08f42db3b83e3a786d66b64c35dc1
Vakarev Maksim <rilst@ya.ru>
2020-10-01 09:42:43 +0300

    Доработал задание

--- Practice/02/C++/Project/Project.cpp
+++ Practice/02/C++/Project/Project.cpp
@@ -5,10 +5,14 @@ using namespace std;
 int main()
 {
     int A;
+    int a;
     double B;
+    double b;
     A = 3.14;
+    a = 3;
     B = 3.14;
-    std::cout << "A = " << A << " B = " << B << endl;
+    b = 3;
+    std::cout << "A = " << A << endl << "a = " << a << endl << "B = " << B << endl << "b = " << b << endl;
     system("pause");
 
 }
