7b95d0be249db27779bd38172fb1dbba9a1f8a72
Vakarev Maksim <rilst@ya.ru>
2020-10-19 16:26:33 +0300

    Practice 14 C++

---
+++ Practice/14/С++/Project/Project/Project.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int n, i, res;
+
+    std::cout << "Введите целое число: ";
+    std::cin >> n;
+    
+    if (n == 0 || n == 1) std::cout << 0 << std::endl;
+    else {
+        res = 0;
+        for (i = 0; i <= n; i++) {
+            if (sqrt(i) == (int)sqrt(i)) res += 1;
+        }
+
+        std::cout << res << std::endl;
+    }
+    system("pause");
+}
