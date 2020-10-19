ed193d7a6f97fe40c7ad7b53802349524ace02ca
Vakarev Maksim <rilst@ya.ru>
2020-10-19 15:29:50 +0300

    Practice 09 C++

--- Practice/09/С++/Project/Project/Project.cpp
+++ Practice/09/С++/Project/Project/Project.cpp
@@ -2,5 +2,25 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "RU");
+
+    int h1, m1, h2, m2, p1, p2;
+    char Symbol1, Symbol2;
+
+    std::cout << "Введите время 1: ";
+    std::cin >> h1 >> Symbol1 >> m1;
+    std::cout << "Введите время 2: ";
+    std::cin >> h2 >> Symbol2 >> m2;
+
+    if (Symbol1 == ':' && Symbol2 == ':' && h1 >= 0 && h2 >= 0 && m1 >= 0 && m2 >= 0) {
+
+        p1 = (h1 * 60) + m1;
+        p2 = (h2 * 60) + m2;
+
+        if (abs(p1 - p2) <= 15) std::cout << "Встреча состоится\n";
+        else std::cout << "Встреча не состоится\n";
+    }
+    else std::cout << "Неверный ввод";
+    
+    system("pause");
 }
--- Practice/10/C++/Project/Project/Source.cpp
+++ Practice/10/C++/Project/Project/Source.cpp
@@ -6,25 +6,18 @@ int main() {
 	int l1, l2, r1, r2;
 
 	std::cout << "Введите 5 целых чисел через пробел: "; std::cin >> s >> l1 >> r1 >> l2 >> r2;
-	
-	if (l1 > r1 || l2 > r2)
-	{
-		std::cout << "Неправильный ввод\n";
-		system("pause");
-		return 0;
-	}
 
-	if (l1 + l2 == s) {
-		std::cout << l1 << " " << l2 << "\n";
+	if (s - l1 < l2) {
+		std::cout << "-1" << "\n";
 	}
-	else if (l1 + r2 == s) {
-		std::cout << l1 << " " << r2 << "\n";
+	else if (s - l1 > l2 && s - l1 <= r2 ) {
+		std::cout << l1 << " " << s - l1 << "\n";
 	}
-	else if (l2 + r1 == s) {
-		std::cout << l2 << " " << r1 << "\n";
+	else if (s - r1 > r2) {
+		std::cout << "-1" << "\n";
 	}
-	else if (r1 + r2 == s) {
-		std::cout << r1 << " " << r2 << "\n";
+	else if (s - r1 <= r2) {
+		std::cout << s - r2 << " " << r2 << "\n";
 	}
 	else std::cout << "-1\n";
 
