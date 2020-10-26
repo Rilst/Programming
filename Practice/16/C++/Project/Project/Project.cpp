76f78957bc3191d106c61ff6676fd766d52bb2e1
Vakarev Maksim <rilst@ya.ru>
2020-10-26 10:04:41 +0300

    Practice 16 C++ v2

--- Practice/16/C++/Project/Project/Project.cpp
+++ Practice/16/C++/Project/Project/Project.cpp
@@ -14,17 +14,21 @@ int main()
 
     for (int i = 0; i < n; i++) {
         std::cin >> bilet;
+        if (strlen(bilet) == 9) {
+            while (d <= 8) {
+                buf[s] = bilet[d];
+                s++;
+                d++;
+            }
 
-       while (d <= 8) {
-            buf[s] = bilet[d];
-            s++;
-            d++;
+            if ((bilet[0] == 'a') && (atoi(buf) == 55661)) {
+                std::cout << bilet << "\n";
+                check = 1;
+            }
         }
-
-       if ((bilet[0] == 'a') && (atoi(buf) == 55661)) { 
-           std::cout << bilet << "\n";
-           check = 1;
-       }
+        else {
+            std::cout << "Неверно введен билет №" << i+1 << "!\n";
+        }
     }
     if (check == 0) std::cout << -1 << '\n';
     system("pause");
