fcab82adfcb24cf8b94e2f9f8a94453adea0cfba
Vakarev Maksim <rilst@ya.ru>
2020-10-29 15:02:55 +0300

    19 C++ /not working

---
+++ Practice/19/C++/Project/Project/Project.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int main()
+{
+	int n, d;
+	string k;
+	string k2, k3;
+	vector<string> mass(256);
+	char symbols;
+	
+	cin >> n;
+	cin >> k;
+	k2 = k;
+	d = k.length();
+	if (n < 1 && n > 8 && n > int(k.length())) {
+		cout << "Неправильный ввод данных\n";
+		system("pause");
+		return 0;
+	}
+	else {
+		if (int(k.length()) < n) {
+			for (int i = 0; i < d; i++) {
+				sort(k2.begin(), k2.end());
+				do
+				{	
+					//for (int j = 0; j < k2.length(); j++) {
+						mass[i] = k2;
+				//	}
+	
+					k += k2;
+					sort(k.begin(), k.end());
+					do
+					{
+						//cout << k << '\n';
+					} while (next_permutation(k.begin(), k.end()));
+				} while (next_permutation(k2.begin(), k2.end()));
+			}
+		}
+		else {
+			sort(k.begin(), k.end());
+			do
+			{
+				cout << k << '\n';
+			} while (next_permutation(k.begin(), k.end()));
+		}
+		for (int j = 0; j < 20; j++) {
+			std::cout << mass[j] << endl;
+		}
+
+	}
+
+
+
+}
