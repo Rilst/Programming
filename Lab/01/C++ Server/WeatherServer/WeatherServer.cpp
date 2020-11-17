06ebcb1d34c84773dacad09c7b5a9e2994109ec4
Vakarev Maksim <rilst@ya.ru>
2020-11-18 01:56:14 +0300

    C++ Server Добавил коменты

--- Lab/01/C++ Server/WeatherServer/WeatherServer.cpp
+++ Lab/01/C++ Server/WeatherServer/WeatherServer.cpp
@@ -42,24 +42,24 @@ string GetTime() {
 
 int main()
 {
-   json raw = json::parse(GetWeather());
+   json raw = json::parse(GetWeather()); // Получаем json погоды
    int BufTime = 0;
    int BufTime2 = 0;
    json RES;
 
    ifstream File("widget_template.html"); // Шаблон виджета
    string str;
-   getline(File, str, '\0');
+   getline(File, str, '\0'); // Читаем шаблон в строку
 
-   Server Weather;
+   Server Weather;  // стартуем сервер
    Weather.Get("/raw", [&](const httplib::Request&, httplib::Response& res) { // При открытие /raw
-       json time = json::parse(GetTime());
-       if (int(time["unixtime"]) < BufTime);    // Чтобы не проходить каждый раз 48 раз по циклу
+       json time = json::parse(GetTime()); // Получаем json времени
+       if (int(time["unixtime"]) < BufTime);    // Чтобы не проходить каждый раз по циклу
        else {
            for (int i = 0; i < raw["hourly"].size(); i++) {
-               if (int(time["unixtime"]) < int(raw["hourly"][i]["dt"])) {
-                   RES.push_back({ {"Temperature", raw["hourly"][i]["temp"]},  {"Weather", raw["hourly"][i]["weather"][0]["description"] } }); // кидаем json
-                   BufTime = raw["hourly"][i]["dt"];
+               if (int(time["unixtime"]) < int(raw["hourly"][i]["dt"])) { // Сравниваем время
+                   RES.push_back({ {"Temperature", raw["hourly"][i]["temp"]},  {"Weather", raw["hourly"][i]["weather"][0]["description"] } }); // создаем конечный json
+                   BufTime = raw["hourly"][i]["dt"]; // кэшируем отдельно время
                    break;
                }
                else {
@@ -68,32 +68,32 @@ int main()
                }
            }
        }
-        res.set_content(RES.dump(), "text/json");
+        res.set_content(RES.dump(), "text/json");   // отдаем итоговый json
         });
-   Weather.Get("/", [&](const httplib::Request&, httplib::Response& res) {
-       json time = json::parse(GetTime());
-       if (int(time["unixtime"]) < BufTime2);
+   Weather.Get("/", [&](const httplib::Request&, httplib::Response& res) { // При открытие корня
+       json time = json::parse(GetTime());// Получаем json времени
+       if (int(time["unixtime"]) < BufTime2); // Чтобы не проходить каждый раз по циклу
        else {
-           for (int i = 0; i < raw["hourly"].size(); i++) {
-               if (int(time["unixtime"]) < int(raw["hourly"][i]["dt"])) {
+           for (int i = 0; i < raw["hourly"].size(); i++) { 
+               if (int(time["unixtime"]) < int(raw["hourly"][i]["dt"])) { // Сравниваем время
 
-                   str.replace(str.find("{hourly[i].temp}"), size("{hourly[i].temp}")-1, to_string(int(round(raw["hourly"][i]["temp"].get<float>()))));
-                   str.replace(str.find("{hourly[i].temp}", size("{hourly[i].temp}")), size("{hourly[i].temp}") - 1, to_string(int(round(raw["hourly"][i]["temp"].get<float>()))));
-                   str.replace(str.find("{hourly[i].weather[0].description}"), size("{hourly[i].weather[0].description}") - 1, raw["hourly"][i]["weather"][0]["description"].get<string>());
-                   str.replace(str.find("{hourly[i].weather[0].icon}"), size("{hourly[i].weather[0].icon}") - 1, raw["hourly"][i]["weather"][0]["icon"].get<string>());
+                   str.replace(str.find("{hourly[i].temp}"), size("{hourly[i].temp}")-1, to_string(int(round(raw["hourly"][i]["temp"].get<float>())))); // Находим в строке первую встречу с временем, заменяем на элемент из json
+                   str.replace(str.find("{hourly[i].temp}", size("{hourly[i].temp}")), size("{hourly[i].temp}") - 1, to_string(int(round(raw["hourly"][i]["temp"].get<float>())))); // Находим в строке вторую встречу с временем, заменяем на элемент из json
+                   str.replace(str.find("{hourly[i].weather[0].description}"), size("{hourly[i].weather[0].description}") - 1, raw["hourly"][i]["weather"][0]["description"].get<string>()); // Аналогично предыдущему
+                   str.replace(str.find("{hourly[i].weather[0].icon}"), size("{hourly[i].weather[0].icon}") - 1, raw["hourly"][i]["weather"][0]["icon"].get<string>()); // Аналогично предыдущему
                   
-                   BufTime2 = raw["hourly"][i]["dt"];
+                   BufTime2 = raw["hourly"][i]["dt"]; // кэшируем время
                    break;
                }
                else {
-                   raw = json::parse(GetWeather());
+                   raw = json::parse(GetWeather()); // если устарел кэш
                    i = 0;
                }
            }
        }
-       res.set_content(str, "text/html");
+       res.set_content(str, "text/html"); // отдаем измененный шаблон
        });
    std::cout << "Start server... OK\n";
-   Weather.listen("localhost", 3000);
+   Weather.listen("localhost", 3000); // слушаем локалку на 3000 порте
 
 }
