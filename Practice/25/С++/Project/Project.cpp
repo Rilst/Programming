f90b6f0064926b2bc011feff77a00aa000e02868
Vakarev Maksim <rilst@ya.ru>
2020-12-03 14:22:47 +0300

    C++ 25 \edit

---
+++ Practice/25/С++/Project/Project.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+
+std::vector<int> BozoSort(std::vector<int> Num, bool big = true);
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::vector<int> Num(n);
+    std::vector<int> Res;
+    for (int i = 0; i < n; i++) {
+        std::cin >> Num[i];
+    }
+    Res = BozoSort(Num);
+    for (int i = 0; i < Res.size(); i++) {
+        std::cout << Res[i] << " ";
+    }
+}
+
+std::vector<int> BozoSort(std::vector<int> Num, bool big) {
+    int b = 0;
+    int n = rand() % Num.size();
+    int tempN, temp;
+    while (true)
+    {
+        if (big == true) {
+            for (int i = 0; i < int(Num.size() - 1); i++) {
+                if (Num[i] > Num[i + 1]) b += 1;
+            }
+            tempN = n;
+            temp = Num[n];
+            n = rand() % Num.size();
+            Num[tempN] = Num[n];
+            Num[n] = temp;
+
+            if (b == Num.size()) {
+                return Num;
+            }
+        }
+    }
+}
