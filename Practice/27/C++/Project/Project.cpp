da1716f149997cc8a580cc1f97f3dab321dbb0b2
Vakarev Maksim <rilst@ya.ru>
2020-12-11 01:53:52 +0300

    Add 27-28 C++

---
+++ Practice/27/C++/Project/Project.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void TopFive(std::vector<int> Array);
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::vector<int> Nums(n);
+    
+    for (int i = 0; i < n; i++) {
+        std::cin >> Nums[i];
+    }
+
+    TopFive(Nums);
+
+}
+void VectorPrnt(std::vector<int> Num) {
+    for (auto i = Num.begin(); i < Num.end(); ++i) {
+        std::cout << *i << " ";
+    }
+    std::cout << "\n";
+}
+std::vector<int> BublSort(std::vector<int> Array) {
+    for (int i = 0; i < int(Array.size() - 1); i++) {
+        for (int j = int(Array.size() - 1); j > i; j--)
+        {
+            if (Array[j - 1] < Array[j])
+            {
+                int buf = Array[j - 1];
+                Array[j - 1] = Array[j];
+                Array[j] = buf;
+            }
+        }
+    }
+    return Array;
+}
+void TopFive(std::vector<int> Array) {
+    std::vector<int> Buf;
+    int k, D = 0;
+    for (int i = 0; i < Array.size(); i++) {
+        if ((int)Buf.size() == 5) {
+            for (int j = 0; j < Buf.size(); j++) {
+                if (Buf[j] > Array[i]) {
+                    k = j;
+                    D = 1;
+                }
+            }
+            if (D == 1) {
+                Buf[k] = Array[i];
+                D = 0;
+            }
+        }
+        else {
+            Buf.push_back(Array[i]);
+        }
+        VectorPrnt(BublSort(Buf));
+
+    }
+}
---
+++ Practice/28/C++/Project/Project.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+
+void print_factorization(unsigned int n);
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int N;
+    std::cout << "Введите целое число больше 1: ";
+    std::cin >> N;
+    print_factorization(N);
+}
+
+void print_factorization(unsigned int n) {
+    std::map <int, int> Res;
+    std::vector<int> K;
+    int D = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        if (n % i == 0) {
+            D++;
+            if (n / i != 1) {
+                K.push_back(i);
+                if (D > 0) {
+                    n = n / i;
+                    i = 1;
+                    D = 0;
+                    continue;
+                }
+            }
+            else K.push_back(i);
+        }
+    }
+    
+    for (auto i : K) Res[i]++;
+
+    int r = 0;
+    for (auto i : Res) {
+        if (int(Res.size()) > 1) {
+            if (i.second != 1) std::cout << i.first << "^" << i.second;
+            if (r == 0) {
+                std::cout << i.first;
+                r++;
+                continue;
+            }
+            if(r > 0) std::cout << "*" << i.first;
+        }
+        else {
+            if (i.second != 1) std::cout << i.first << "^" << i.second;
+            else std::cout << i.first;
+        }
+
+    }
+}
