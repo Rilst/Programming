82025375013557a622cd6fe5412438f6d6d4d434
Vakarev Maksim <rilst@ya.ru>
2020-12-17 02:01:38 +0300

    end coding

--- Lab/02/C++/ServerAlice/ServerAlice.cpp
+++ Lab/02/C++/ServerAlice/ServerAlice.cpp
@@ -8,6 +8,7 @@
 using namespace httplib;
 using json = nlohmann::json;
 
+
 json Create(std::string str, json state, bool session, json buttons) {
     json Responce;
     Responce["response"]["text"] = str;
@@ -29,6 +30,11 @@ std::vector<std::string> GetCommand(json token) {
     return Res;
 }
 
+std::string replace(std::string str, std::string source, std::string get) {
+    int start = str.find(source);
+    return str.replace(start, source.length(), get);
+}
+
 json GetMain(json req) {
     json State;
     State["Silence"] = false;
@@ -37,20 +43,13 @@ json GetMain(json req) {
 
     std::string STR = u8"Здравствуйте! Я помогу вам с покупками.";
 
-    json Btns;
-    if (req["state"]["session"]["Silence"] == false) {
-        Btns[0]["title"] = u8"Молчать";
-        Btns[0]["payload"]["Silence"] = true;
-    }
-    else if (req["state"]["session"]["Silence"] == true) {
-        Btns[0]["title"] = u8"Говорить";
-        Btns[0]["payload"]["Silence"] = false;
-    }
-
+    json Btns = json::array();
+    Btns[0]["title"] = u8"Молчать";
+    Btns[0]["payload"]["Silence"] = true;
     Btns[1]["title"] = u8"Помощь";
     Btns[1]["payload"]["Mode"] = "Help";
 
-    json ActiveBtns;
+    json ActiveBtns = json::array();
     ActiveBtns[0]["title"] = u8"Добавить в корзину";
     ActiveBtns[0]["payload"]["Help"] = "Add";
     ActiveBtns[1]["title"] = u8"Очистить корзину";
@@ -72,19 +71,30 @@ json GetMain(json req) {
         return Create(STR, State, false, Btns);
     }
 
+
     State["Silence"] = req["state"]["session"]["Silence"];
     State["Check"] = req["state"]["session"]["Check"];
     State["Mode"] = req["state"]["session"]["Mode"];
 
-    if (req["state"]["session"]["Silence"] == true || req["request"]["payload"]["Silence"] == true) {
+    if (req["request"]["payload"]["Silence"] == true) {
+        State["Silence"] = true;
+    }
+    if (req["request"]["payload"]["Silence"] == false) {
+        State["Silence"] = false;
+    }
+
+    if (State["Silence"] == true) {
+        Btns[0]["title"] = u8"Говорить";
+        Btns[0]["payload"]["Silence"] = false;
+    }
+
+    if (State["Silence"] == true) {
         State["Silence"] = true;
         STR = u8"Молчу, молчу";
-        return Create(STR, State, false, Btns);
     }
-    else if (req["state"]["session"]["Silence"] == false || req["request"]["payload"]["Silence"] == false) {
+    else if (State["Silence"] == false) {
         State["Silence"] = false;
         STR = u8"Хорошо";
-        return Create(STR, State, false, Btns);
     }
 
     if (req["session"]["new"] == false) {
@@ -116,6 +126,7 @@ json GetMain(json req) {
                     State["Mode"] = "Main";
                     return Create(STR, State, false, Btns);
                 }
+                else if (req["request"]["payload"]["Mode"] != "Help") STR = u8"Неизвестная команда";
                 return Create(STR, State, false, ActiveBtns);
             }
 
@@ -144,27 +155,68 @@ json GetMain(json req) {
                     STR = u8"Корзина пуста!";
                 }
                 else if (BUF[0] == u8"что" && BUF[1] == u8"в" && BUF[2] == u8"корзине") {
-                    if (State["Check"].empty() == true) {
+                    if (State["Check"].size() <= 0) {
                         STR = u8"Корзина пуста!";
                     }
-                    else if (State["Check"].empty() == false) {
+                    else if (State["Check"].size() > 0) {
                         std::stringstream str;
-                        for (int i = 0; i < State["Check"].size(); i++) {
-                            str << State["Check"][i]["item"] << " " << State["Check"][i]["price"] << " рублей" << "\n";
+                        for (int i = 0; i < State["Check"].size(); i++){
+                            str << std::string(State["Check"][i]["item"]) << u8" " << std::string(State["Check"][i]["price"]) << u8" рублей" << "\n";
                         }
                         STR = str.str();
                     }
                 }
                 else if (BUF.size() == 1 && BUF[0] == u8"сумма") {
                     int sum = 0;
+                    std::string num;
                     for (int i = 0; i < State["Check"].size(); i++) {
-                        sum += int(State["Check"][i]["price"]);
+                        num = State["Check"][i]["price"];
+                        sum += atoi(num.c_str());
                     }
-                    STR = "Сумма всех товаров в корзине: " + sum;
+                    STR = u8"Сумма всех товаров в корзине: " + std::to_string(sum) + u8" рублей";
                 }
                 else if (BUF.size() == 2 && BUF[0] == u8"покупка" && BUF[1] == u8"завершена") {
 
+                    STR = u8"Заходите ещё";
+                    if (State["Check"].size() > 0) {
+                        std::ifstream jsnout("webhooks.json");
+
+                        json Check;
+
+                        Check["check"] = State["Check"];
+                        if (req["session"]["user"]["user_id"] == nullptr) {
+                            Check["user_id"] = "anonymous";
+                            //Check.push_back("{\"user_id\": \"anonymous\"}"_json);
+                        }
+                        else if (req["session"]["user"]["user_id"] != nullptr) {
+                            Check["user_id"] = req["session"]["user"]["user_id"];
+                        }
+
+                        json JArr;
+                        JArr["WebHooks"] = json::array();
+
+                        if (jsnout) {
+                            json j = json::parse(jsnout);
+                            for (int i = 0; i < j["WebHooks"].size(); i++) {
+                                JArr["WebHooks"].push_back(j["WebHooks"][i]);
+                            }
+                            jsnout.close();
+                        }
+
+
+                        for (int i = 0; i < JArr["WebHooks"].size(); i++) {
+                            std::string addr = JArr["WebHooks"][i];
+                            Client cli(addr.c_str());
+                            std::stringstream ss;
+                            std::stringstream ss2;
+                            ss << std::setw(2) << Check;
+                            std::string strstate = ss.str();
+                            auto res = cli.Post("/", strstate, "application/json");
+                        }
+                    }
+                    return Create(STR, State, true, nullptr);
                 }
+                else STR = u8"Неизвестная команда";
                 return Create(STR, State, false, Btns);
             }
         }
@@ -177,10 +229,78 @@ void Get_Session(const Request& req, Response& res) {
     res.set_content(str.str(), "text/json; charset=UTF-8");
 }
 
+void Get_Template(const Request& req, Response& res) {
+    std::ifstream ifstr("temp.html");
+    std::string temp;
+    std::getline(ifstr, temp, '\0');
+    ifstr.close();
+
+
+    std::ifstream iftempl("template.html");
+    std::string templated;
+    std::getline(iftempl, templated, '\0');
+    iftempl.close();
+
+    std::string data = "";
+
+    std::ifstream jsnout("webhooks.json");
+    
+    json JArr;
+    JArr["WebHooks"] = json::array();
+
+    if (jsnout) {
+        json j = json::parse(jsnout);
+        for (int i = 0; i < j["WebHooks"].size(); i++) {
+            JArr["WebHooks"].push_back(j["WebHooks"][i]);
+        }
+        jsnout.close();
+    }
+   
+    for (int i = 0; i < JArr["WebHooks"].size(); i++) {
+        std::string tempM = temp;
+        tempM.replace(tempM.find("Webhook URL"), std::size("Webhook URL") - 1, JArr["WebHooks"][i]);
+        tempM.replace(tempM.find("Webhook URL"), std::size("Webhook URL") - 1, JArr["WebHooks"][i]);
+        data += tempM + "\n";
+    }
+    std::string tempP = templated;
+    tempP.replace(tempP.find("{webhooks_list}"), std::size("{webhooks_list}"), data);
+
+    if (req.method == "POST") {
+        if (req.has_param("del")) {
+            std::string value = req.get_param_value("del");
+            for (int i = 0; i < JArr["WebHooks"].size(); i++) {
+                if (JArr["WebHooks"][i] == value) {
+                    JArr["WebHooks"].erase(JArr["WebHooks"].begin() + i);
+                }
+            }
+            std::stringstream sd;
+            sd << std::setw(2) << JArr;
+
+            std::ofstream kj("webhooks.json");
+            kj << sd.str() << std::endl;
+            kj.close();
+        }
+        if (req.has_param("set")) {
+            std::string value = req.get_param_value("set");
+            JArr["WebHooks"].push_back(value);
+            std::stringstream sd;
+            sd << std::setw(2) << JArr;
+
+            std::ofstream kj("webhooks.json");
+            kj << sd.str() << std::endl;
+            kj.close();
+        }
+        res.set_redirect("#");
+    }
+
+    res.set_content(tempP, "text/html");
+}
 
 int main() {
     Server svr;                            
-    svr.Post("/", Get_Session);           
+    svr.Post("/", Get_Session);      
+    svr.Post("/webhooks", Get_Template);
+    svr.Get("/webhooks", Get_Template);
     std::cout << "Start server... OK\n";
     svr.listen("localhost", 3000);         
 }
---
+++ Lab/02/Python/ClientAlice.py
@@ -0,0 +1,107 @@
+from flask import Flask, request
+from datetime import datetime
+import openpyxl
+from openpyxl.styles import Alignment, Font
+
+Buf_Size = 1
+buf = []
+
+def FindClearRow(page, col):
+    i = 1
+    while True:
+        if type(page[i][col]).__name__ == 'MergedCell':
+            i += 1
+        else:
+            if page[i][col].value != None:
+                i += 1
+            else:
+                break
+    return i
+
+def writeinbuf(Res):
+    global buf
+    global Buf_Size
+    buf.append(Res);
+    buf[-1]["datetime"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    if len(buf) >= Buf_Size:
+        try:
+            book = openpyxl.open("data.xlsx", read_only=False)
+        except:
+            book = openpyxl.Workbook()
+        page = book.active
+        if page.title != "Alice":
+        
+            page.title = "Alice"
+            page['A1'] = "N"
+            page['B1'] = "User ID"
+            page['C1'] = "Datetime"
+            page['D1'] = "Item"
+            page['E1'] = "Price"
+            
+            page.column_dimensions['A'].width = 5
+            page.column_dimensions['B'].width = 67
+            page.column_dimensions['C'].width = 18
+            page.column_dimensions['D'].width = 30
+            page.column_dimensions['E'].width = 13
+            
+            page[1][0].alignment = Alignment(horizontal='center', vertical='center')
+            page[1][1].alignment = Alignment(horizontal='center', vertical='center')
+            page[1][2].alignment = Alignment(horizontal='center', vertical='center')
+            page[1][3].alignment = Alignment(horizontal='center', vertical='center')
+            page[1][4].alignment = Alignment(horizontal='center', vertical='center')
+            page[1][0].font = Font(bold=True)
+            page[1][1].font = Font(bold=True)
+            page[1][2].font = Font(bold=True)
+            page[1][3].font = Font(bold=True)
+            page[1][4].font = Font(bold=True)
+            
+        for i in range(len(buf)):
+        
+            N = FindClearRow(page, 0)
+            OLD = N
+            while True:
+                N -= 1
+                if type(page[N][0]).__name__ == 'Cell':
+                    if str(page[N][0].value).isdigit():
+                        page[OLD][0].value = page[N][0].value + 1
+                        page[OLD][0].alignment = Alignment(horizontal='center', vertical='center')
+                        break
+                    else:
+                        page[OLD][0].value = 1
+                        page[OLD][0].alignment = Alignment(horizontal='center', vertical='center')
+                        break
+                
+            UID = FindClearRow(page, 1)
+            page[UID][1].value = buf[i]["user_id"]
+            page[UID][1].alignment = Alignment(horizontal='center', vertical='center')
+            
+            DateT = FindClearRow(page, 2)
+            page[DateT][2].value = buf[i]["datetime"]
+            page[DateT][2].alignment = Alignment(horizontal='center', vertical='center')
+            
+            Leng = len(buf[i]["check"])
+            page.merge_cells(start_row=OLD, start_column=1, end_row=OLD+Leng-1, end_column=1)
+            page.merge_cells(start_row=OLD, start_column=2, end_row=OLD+Leng-1, end_column=2)
+            page.merge_cells(start_row=OLD, start_column=3, end_row=OLD+Leng-1, end_column=3)
+            
+            for K in range(len(buf[i]["check"])):
+                Itm = FindClearRow(page, 3)
+                page[Itm][3].value = buf[i]["check"][K]["item"]
+                page[Itm][3].alignment = Alignment(horizontal='center', vertical='center')
+                page[Itm][4].value = int(buf[i]["check"][K]["price"])
+                page[Itm][4].alignment = Alignment(horizontal='center', vertical='center')    
+        book.save("data.xlsx")
+        buf = []
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def index():
+    if request.method == 'POST':
+        if request.is_json:
+            Res = request.get_json()
+            writeinbuf(Res)
+            return "OK"
+ 
+if __name__ == "__main__":
+    app.run()
